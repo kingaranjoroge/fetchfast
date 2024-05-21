1a7eb9a34511dcddcfd367d68d920f5da9f20774
kingaranjoroge <briankingara4@gmail.com>
2024-05-21 17:36:28 +0300

    change type of request from string to Request

--- app/api/categories/[id]/route.ts
+++ app/api/categories/[id]/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server';
 import connectMongoDB from '@/libs/mongodb';
 import Category from '@/models/categories';
 
-export async function GET(request: string, { params }) {
+export async function GET(request: Request , { params }) {
   await connectMongoDB()
 
   const { id } = params
