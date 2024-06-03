b77901195ed33b70396f03e7b45a2f42e4df3af5
kingaranjoroge <briankingara4@gmail.com>
2024-06-03 23:23:39 +0300

    add soups & stews route

---
+++ app/categories/soups&stews/loading.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Loading = () => {
+  return (
+    <main className="flex flex-grow justify-center items-center">
+        <h1>Loading...</h1>
+    </main>
+  )
+}
+
+export default Loading
