0b842d1cc52d8f1bcd318b4c2096eb459f20f9f8
kingaranjoroge <briankingara4@gmail.com>
2024-08-03 13:18:55 +0300

    modified category components card

--- app/categories/page.tsx
+++ app/categories/page.tsx
@@ -2,20 +2,32 @@ import React from 'react'
 import Link from 'next/link'
 import CategoryComponent from '@/components/CategoryComponent'
 import Image from 'next/image'
-import backgroundImage from '../../public/categorybg2.jpg'
 import Nav from '@/components/Nav'
+import appetizers from '../../public/categorycards-bg/appetizers.png'
 
 const Categories = () => {
   return (
-    <main className="flex flex-grow justify-center items-center flex-col bg-category-background bg-cover bg-center bg-fixed">
+    <main className="flex flex-grow justify-center items-center flex-col">
       <Nav />
       <section className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 sm:gap-x-16 gap-8 place-content-center pt-20 pb-4">
-        <Link href="/categories/appetizers"><CategoryComponent name='Appetizers' /></Link>        
-        <Link href="/categories/beverages"><CategoryComponent name='Beverages' /></Link>        
-        <Link href="/categories/desserts"><CategoryComponent name='Desserts' /></Link>        
-        <Link href="/categories/main-courses"><CategoryComponent name='Main Courses' /></Link>        
-        <Link href="/categories/salads"><CategoryComponent name='Salads' /></Link>        
-        <Link href="/categories/soups&stews"><CategoryComponent name='Soups & Stews' /></Link>                
+        <Link href="/categories/appetizers">
+          <CategoryComponent name='Appetizers' backgroundImage='categorycards-bg/appetizers.png' />
+        </Link>        
+        <Link href="/categories/beverages">
+          <CategoryComponent name='Beverages' backgroundImage='categorycards-bg/beverages.png' />
+        </Link>        
+        <Link href="/categories/desserts">
+          <CategoryComponent name='Desserts' backgroundImage='categorycards-bg/desserts.png' />
+        </Link>        
+        <Link href="/categories/main-courses">
+          <CategoryComponent name='Main Courses' backgroundImage='categorycards-bg/main-courses.png' />
+        </Link>        
+        <Link href="/categories/salads">
+          <CategoryComponent name='Salads' backgroundImage='categorycards-bg/salads.png' />
+        </Link>        
+        <Link href="/categories/soups&stews">
+          <CategoryComponent name='Soups & Stews' backgroundImage='categorycards-bg/soups-stews.png' />
+        </Link>                
       </section>
     </main>
   )
--- components/CategoryComponent.tsx
+++ components/CategoryComponent.tsx
@@ -1,27 +1,19 @@
 import React from 'react'
-import Image from 'next/image'
-// import categorybg from '../public/categorycardbg.jpg'
 
 interface Props {
   name: string;
+  backgroundImage: string;
 }
 
-const CategoryComponent = ({name}: Props) => {
+const CategoryComponent = ({ name, backgroundImage }: Props) => {
   return (
-    <div className="card w-64 h-40 bg-slate-500 text-primary-content bg-opacity-70">
-      {/* <Image
-        alt="Background Image"
-        src={categorybg}
-        placeholder="blur"
-        quality={100}
-        fill
-        sizes="100vw"
-        style={{
-          objectFit: 'cover',
-        }}
-      /> */}
-      <div className="card-body justify-center items-center z-10">
-        <h2 className="card-title">{name}</h2>
+    <div 
+      className="card w-64 h-40 bg-slate-500 text-primary-content relative overflow-hidden transform transition-transform duration-300 hover:scale-105" 
+      style={{ backgroundImage: `url(${backgroundImage})`, backgroundSize: 'cover', backgroundPosition: 'center' }}
+    >
+      <div className="absolute inset-0 bg-black bg-opacity-50"></div>
+      <div className="card-body justify-center items-center z-10 relative">
+        <h2 className="card-title text-slate-200">{name}</h2>
       </div>
     </div>
   )
