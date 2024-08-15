00c79175dadfa8694864c55eb9ad41db5756e8f7
kingaranjoroge <briankingara4@gmail.com>
2024-08-15 23:02:51 +0300

    change rendering behaviour

--- components/Auth.tsx
+++ components/Auth.tsx
@@ -3,13 +3,20 @@
 import { useSession } from "next-auth/react"
 import GoogleAuth from "./GoogleAuth"
 import { useRouter } from "next/navigation"
+import { useEffect } from "react"
 
 export default function Auth() {
     const router = useRouter()
-    const { status, data: session } = useSession()
+    const { status } = useSession()
+
+    useEffect(() => {
+        if (status === "authenticated") {
+            router.push('/categories')
+        }
+    }, [status, router])
 
     if (status === "authenticated") {
-        router.push('/categories')
+        return null; // or a loading spinner if you prefer
     } else {
         return <GoogleAuth />
     }
