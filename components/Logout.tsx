c8ca6e30827757946d04a0fd81b3e05c91afab66
kingaranjoroge <briankingara4@gmail.com>
2024-08-16 18:33:07 +0300

    add logout functionality if signed in with google

--- components/Logout.tsx
+++ components/Logout.tsx
@@ -1,9 +1,15 @@
-// import { AppRouterInstance, NextRouter } from "next/dist/shared/lib/app-router-context.shared-runtime";
 import { NextRouter } from "next/router";
+import { signOut } from "next-auth/react";
 
-export const logout = (router: NextRouter) => {
+export const logout = async (router: NextRouter) => {
   // Clear user data from localStorage or any other storage mechanism you're using
   localStorage.removeItem('user');
+
+  // Sign out using next-auth
+  await signOut({
+    callbackUrl: '/', // Redirect to the homepage after sign out
+    redirect: false, // Prevent automatic redirection by next-auth
+  });
 
   // Check if the current path is the homepage
   if (router.pathname === '/') {
