a239f5d853bf1e546cf8f6dc2cf5973112f38a6f
kingaranjoroge <briankingara4@gmail.com>
2025-04-12 00:41:42 +0300

    add admin dashboard

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,7 +1,22 @@
-export default function Layout({ children} : {children: React.ReactNode}) {
-    return (
-        <main>
+import Sidebar from "@/components/admin/Sidebar"
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="h-full relative">
+      {/* Mobile header with menu button */}
+      {/* <div className="md:hidden flex items-center justify-between p-4 border-b">
+        <h1 className="text-xl font-semibold">Admin Dashboard</h1>
+      </div> */}
+
+      {/* large screens sidebar and main area implementation */}
+      <div className="flex h-full w-full">
+        <Sidebar className="h-full" />
+        <main className="flex-1 overflow-y-auto">
+          <div className="container mx-auto p-4 md:p-6">
             {children}
+          </div>
         </main>
-    )
+      </div>
+    </div>
+  )
 }
--- components/Nav.tsx
+++ components/Nav.tsx
@@ -40,6 +40,7 @@ const Nav = () => {
             <Link href="/" className="btn btn-ghost normal-case text-xl text-textdark font-bold">FetchFast</Link>
         </div>
         <div className="navbar-end">
+            <Link href="/admin">Admin</Link>
             <ThemeSwitch />
             <button className="btn btn-ghost btn-circle">
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
